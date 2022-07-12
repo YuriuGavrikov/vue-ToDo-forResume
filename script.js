f5a12b00340c481637797c48a3800782fa69e6b9
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2022-07-12 18:55:35 +0300

    create app ToDo

---
+++ script.js
@@ -0,0 +1,21 @@
+const App = {
+   data() {
+      return {
+         placeholderString: 'Введите название заметки',
+         title: 'Список заметок',
+         inputValue: '',
+         notes: ['Заметка 1', 'Заметка 2', 'Заметка 3']
+      }
+   },
+   methods: {
+      inputChangeHandler(event) {
+         this.inputValue = event.target.value
+      },
+      addNewNote() {
+         this.notes.push(this.inputValue)
+         this.inputValue = ""
+      }
+   }
+}
+
+Vue.createApp(App).mount('#app')
